57df2af6113b9fbb2c64013a05a7856791c3d341
marshal <mail.rahkar.co>
2022-08-21 15:46:16 +0430

    Initial commit.

---
+++ BarberShopReservation/src/main/kotlin/com/github/alirezatayefeh/BarberShopReservationApplication.kt
@@ -0,0 +1,11 @@
+package com.github.alirezatayefeh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BarberShopReservationApplication
+
+fun main(args: Array<String>) {
+	runApplication<BarberShopReservationApplication>(*args)
+}
---
+++ BarberShopReservation/src/test/kotlin/com/github/alirezatayefeh/BarberShopReservationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.alirezatayefeh
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BarberShopReservationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
