50bcbb1408fc853b695fd2e2bd8c508ade613537
alirezatayefeh <tayefehalireza@gmail.com>
2022-09-10 18:48:36 +0430

    Fix setTime and ReserveDto.

--- BarberShopReservation/src/main/kotlin/com/alirezatayefeh/barbershopreservation/repository/AdminRepository.kt
+++ BarberShopReservation/src/main/kotlin/com/alirezatayefeh/barbershopreservation/repository/AdminRepository.kt
@@ -1,7 +1,13 @@
 package com.alirezatayefeh.barbershopreservation.repository
 
+import com.alirezatayefeh.barbershopreservation.model.ReserveEntity
+import com.alirezatayefeh.barbershopreservation.model.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AdminRepository {
+interface AdminRepository : JpaRepository<ReserveEntity, Int>{
+    @Query("from ReserveEntity r where r.deletionTimestamp = 0")
+    fun findReserveTime(): List<UserEntity>
 }
--- BarberShopReservation/src/main/kotlin/com/alirezatayefeh/barbershopreservation/service/AdminService.kt
+++ BarberShopReservation/src/main/kotlin/com/alirezatayefeh/barbershopreservation/service/AdminService.kt
@@ -1,15 +1,17 @@
 package com.alirezatayefeh.barbershopreservation.service
 
+import com.alirezatayefeh.barbershopreservation.model.UserDto
 import com.alirezatayefeh.barbershopreservation.repository.AdminRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 
 @Service
 class AdminService @Autowired constructor(
     private val adminRepository: AdminRepository,
 ) {
-    fun getAllReserveTimes(offset: Int, limit: Int) {
-        TODO("Not yet implemented")
+    fun getAllReserveTimes(offset: Int, limit: Int): List<UserDto> {
+        return adminRepository.findReserveTime().map { it.toDto() }
     }
 
     fun openShop() {
